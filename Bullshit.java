2bee480b8527d5750509edbb0cdc3788bb17d087
Felix Ali <fealipaz@aol.de>
2015-07-05 18:29:45 +0200

    Initial Project V

---
+++ Bullshit.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Bullshit{
+public static void main(String[] args) {
+ System.out.println("Please only press y for yes and n for no");
+ System.out.println("Do yo want to begin the adeventure ?");
+ Scanner sc = new Scanner(System.in);
+   String i1 = sc.nextLine();
+
+   if(i1.equals("y")){
+   	System.out.println("Alright than lets begin");
+    System.out.println("Do you like cocks ?");
+
+    Scanner sc2 = new Scanner(System.in);
+    String i2 = sc.nextLine();
+    if(i2.equals("y")){
+    	System.out.println("HA GAYYYY");
+    }
+    else if(i2.equals("n")){
+   	System.out.println("So you like pussy ?");
+   	System.out.println("Than flick my bean");
+   }
+
+}
+      else{
+      	System.out.println("Only y or n you dump fuck");
+   }
+}
+  }
+
+
+
+
+
